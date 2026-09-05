f882c2e1174c6f13108ff92e9521eeb4de3e63c3
bananaapache <110048127+BananaApache@users.noreply.github.com>
2026-09-05 12:27:21 -0400

    add more fastapi

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 
+from enum import Enum
+
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -9,5 +12,61 @@ async def root():
     return {"message": "Hello World"}
 
 @app.get("/items/{item_id}")
-async def read_item(item_id):
+async def read_item(item_id: int):
     return {"item_id": item_id}
+
+@app.get("/users/me") # order matters
+async def read_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def read_user(user_id: str):
+    return {"user_id": user_id}
+
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name": model_name, "message": "Have some residuals"}
+
+#? QUERY PARAMS
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+@app.get("/query")
+async def query(skip: int = 0, limit: int = 10): # can add queries http://127.0.0.1:8000/items/?skip=0&limit=10
+    return fake_items_db[skip : skip + limit]
+
+#? QUERY AND BODY PARAMS
+
+@app.get("/users/{user_id}/items/{item_id}")  # http://127.0.0.1:8000/users/444/items/2?q=poo&short=true
+async def read_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
+
+
+#? BODY PARAMS
+
+class Item(BaseModel):
+    item_name: str
+    description: str | None = None
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
